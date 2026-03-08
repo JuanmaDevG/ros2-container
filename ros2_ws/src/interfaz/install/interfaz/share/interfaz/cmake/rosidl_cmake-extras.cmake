46e79c2654bc57088ee5747cba6792434dc0ea8d
JuanmaDevG <juanmagonzalezmartinez@protonmail.com>
2026-03-08 21:38:29 +0100

    Accidentally uploaded binaries and now this is a correction

--- ros2_ws/src/interfaz/install/interfaz/share/interfaz/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(interfaz_IDL_FILES "msg/MiMensaje.idl;msg/P2pkgMensaje.idl;action/Fibonacci.idl")
-set(interfaz_INTERFACE_FILES "msg/MiMensaje.msg;msg/P2pkgMensaje.msg;action/Fibonacci.action")
